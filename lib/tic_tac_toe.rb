9d9ada3e26c5a5e40ae02657f9f13c7e2e818b3c
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-12 04:51:13 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
-require 'pry'
 class TicTacToe
-  WIN_COMBINATIONS = 
+  
+  WIN_COMBINATIONS =
     [
       [0,1,2],
       [3,4,5],
@@ -11,9 +11,11 @@ class TicTacToe
       [0,4,8],
       [6,4,2]
     ]
-  def initialize(board = Array.new(9, " "))
-    @board = board
+    
+  def initialize
+    @board = Array.new(9, " ")
   end
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -21,22 +23,73 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
   def input_to_index(str)
-    @str = str
-    @str = @str.to_i
-    @pic = @str - 1
+    str = str.to_i
+    pic = str - 1
     #@pic = pic
-#binding.pry
   end
-  def move(pic, token)
-    @board[0] = "X"
-    @board[4] = "O"
+  
+  def move(pic, token = "X")
+    @board[pic] = token
   end
   
   def position_taken?(pic)
-    @board[pic].empty?
+    if @board[pic] == " "
+       false
+    else
+       true
+    end
+    #binding.pry
+  end 
+  
+  def valid_move?(pic)
+   if pic >= 0 && pic < 9
+     !position_taken?(pic)
+   else
+     false
+    end
   end
-#binding.pry
   
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
   
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def turn
+    puts "Please choose your move by choosing 1-9."
+    str = gets
+    
+      p1 = input_to_index(str)
+    if valid_move?(p1)
+      @board[p1] = current_player
+      display_board
+      
+    else
+      puts "invalid"
+      # puts "Please choose your move by choosing 1-9."
+      str = gets
+    end
+    #turn
+  end
+  
+  def won?
+    WIN_COMBINATIONS.find do|w|
+    @board[w[0]] == @board[w[1]] && @board[w[1]] == @board[w[2]]
+    end
+  end
+  
+  def full?
+    @board.all?{|s|s != " "}
+  end
+
+  def draw?
+    if self.won? == false && self.full?
+  
+    end
+  end
+  #binding.pry
 end
